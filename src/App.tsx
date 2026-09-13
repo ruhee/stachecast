b4eb54e5ce1ff280d56f75f4ae22ec6bb269ddf1
Ruhee Dewji <ruhee.dewji@fig.ca>
2026-09-12 21:12:18 -0400

    Run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,44 @@ export function App() {
         <a href="https://baseballsavant.mlb.com/player-scroll/game?gamePk=824958&player_id=656302">
           velo was down
         </a>{" "}
-        and, for the first time in awhile, <a href="https://bloximages.chicago2.vip.townnews.com/daytondailynews.com/content/tncms/assets/v3/editorial/1/a9/1a9e0a1f-68dd-56e0-a2f6-ac8e3af5bb16/6a9f7871e7e50.image.jpg?resize=1763%2C1176">so was his facial hair</a>. What this
-        website asks is: are those things related?
+        and, for the first time in awhile,{" "}
+        <a href="https://bloximages.chicago2.vip.townnews.com/daytondailynews.com/content/tncms/assets/v3/editorial/1/a9/1a9e0a1f-68dd-56e0-a2f6-ac8e3af5bb16/6a9f7871e7e50.image.jpg?resize=1763%2C1176">
+          so was his facial hair
+        </a>
+        . What this website asks is: are those things related?
       </p>
       <div className="chart-container">
-        <p>In 2026, Cease's velo is inversely related to the amount of hair on his face.</p>
+        <p>
+          In 2026, Cease's velo is inversely related to the amount of hair on
+          his face.
+        </p>
         <VeloBarChart />
         <BreakingVeloBarChart />
-        <p>Cease generally has higher game scores with a full beard, though, and all of his best games by game score (<a href="https://www.mlb.com/bluejays/video/dylan-cease-take-no-hitter-into-7th-in-blue-jays-win">a no-hit bid into the 7th</a>, <a href="https://www.mlb.com/video/dylan-cease-flirts-with-no-hitter-in-blue-jays-win">a no-hit bid into the 9th</a>, and <a href="https://www.mlb.com/news/dylan-cease-pitches-complete-game-shutout-al-cy-young-award-case
-        ">a CG shutout</a>) were on full-beard or long-beard days.</p>
+        <p>
+          Cease generally has higher game scores with a full beard, though, and
+          all of his best games by game score (
+          <a href="https://www.mlb.com/bluejays/video/dylan-cease-take-no-hitter-into-7th-in-blue-jays-win">
+            a no-hit bid into the 7th
+          </a>
+          ,{" "}
+          <a href="https://www.mlb.com/video/dylan-cease-flirts-with-no-hitter-in-blue-jays-win">
+            a no-hit bid into the 9th
+          </a>
+          , and{" "}
+          <a
+            href="https://www.mlb.com/news/dylan-cease-pitches-complete-game-shutout-al-cy-young-award-case
+        "
+          >
+            a CG shutout
+          </a>
+          ) were on full-beard or long-beard days.
+        </p>
         <GameScoreBarChart />
-        <p>His strikeouts follow the velo trend a little more, but they're highest with the short beard. (<em>Really</em> small sample size, though: only 3 starts in 2026 with this beard type)</p>
+        <p>
+          His strikeouts follow the velo trend a little more, but they're
+          highest with the short beard. (<em>Really</em> small sample size,
+          though: only 3 starts in 2026 with this beard type)
+        </p>
         <K9BarChart />
       </div>
       <footer>
@@ -43,9 +70,8 @@ export function App() {
             Baseball Savant and Statcast
           </a>
           . Beard data via MLB highlight packages and my eyes. Exceptional
-          product management from{" "}
-          <a href="https://bsky.app/profile/gareths.bsky.social">Gareth</a> and
-          data science advice from{" "}
+          advice by{" "}
+          <a href="https://bsky.app/profile/gareths.bsky.social">Gareth</a> and{" "}
           <a href="https://bsky.app/profile/pointsman.bsky.social">Chris</a>.
           Perfect name thanks to{" "}
           <a href="https://bsky.app/profile/phrichards.ca">Phil</a>.{" "}
